372bf50e0ea2963515d21b7e72cca5b3931c1097
Konstantinos Nitsopoulos <NITSOPOULOS@MAIL.COM>
2021-11-05 16:50:03 +1300

    Update main-RESPONSE_TO_cculianu.cpp

--- src/main-RESPONSE_TO_cculianu.cpp
+++ src/main-RESPONSE_TO_cculianu.cpp
@@ -1,32 +1,49 @@
-#include "crypto/sha256.cpp"    //Copy-pasted from BCHN.
 #include <cstdio>
 #include <mutex>
 #include <thread>
-#include <vector>
+#include "crypto/sha256.cpp"    //From BCHN.
 
+uint8_t hexList[103] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,0,0,0,0,0,0,0,0xa,0xb,0xc,0xd,0xe,0xf,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0xa,0xb,0xc,0xd,0xe,0xf};
+uint8_t* FromHex(std::string Hex){  //Some claim std::stringstream is "slow", so I'm using hexList instead.
+    size_t Len=Hex.length();
+    uint8_t* Return = new uint8_t[(Len+1)/2];
+    for(size_t Ind=0;Ind<Len;Ind+=2) Return[Ind/2]=hexList[(uint8_t)Hex[Ind]]<<4 | hexList[(uint8_t)Hex[Ind+1]];
+    if (Len%2) Return[(Len-1)/2]=hexList[(uint8_t)Hex.back()];
+    return Return;
+}
 std::once_flag EnsureOnly1Exit;   // ensure that if 2 threads happen to find a solution simultaneously, only 1 wins
-void Exit(const std::vector<uint8_t> &txn){
-    for (uint8_t byte : txn) std::printf("%02x", byte);
+void Exit(const uint8_t* TXn,const size_t &TXSize){
+    for (size_t Ind=0;Ind<TXSize;Ind++) std::printf("%02x", TXn[Ind]);
     exit(0);
 }
-void Hasher(uint8_t ThreadN, size_t nThreads, const size_t &Pos, const std::vector<uint8_t> &Pattern, const bool &PatternOdd, std::vector<uint8_t> TXn) {
-    const size_t TXSize=TXn.size();
-    bool Finished;
+void Hasher(int ThreadN,char **argv) {
+    size_t ThreadsN=FromHex(argv[1])[0]+1;
+    uint8_t* NoncePos=FromHex(argv[2]);
+    const size_t Pos=NoncePos[0]<<16 | NoncePos[1]<<8 | NoncePos[2];
+
+    const int8_t PatternLen=std::string(argv[3]).length();
+    const uint8_t* Pattern=FromHex(argv[3]);
+
+    const size_t TXSize=std::string(argv[4]).length()/2;
+    uint8_t* TXn=FromHex(argv[4]);
+
+    bool Finished;  //Variable initializations.
+    int8_t CheckByte;
     uint8_t SHA256[32];
     CSHA256 SHA256C;
     TXn[Pos]=ThreadN;   //This byte encodes the winning thread.
-    do{do{do{do{do{do{do{do{    //'for' loops can't get to byte value 255, and therefore lie about the hash rate by like 1%.
-        SHA256C.Write(TXn.data(), TXSize);
+    do{do{do{do{do{do{do{do{    //'for' loops can't get to byte value 255, and cause incorrect hash rate calculations.
+        SHA256C.Write(TXn,TXSize);
         SHA256C.Finalize(SHA256);
         SHA256C.Reset();
-        SHA256C.Write(SHA256,32);
+        SHA256C.Write(SHA256,0x20);
         SHA256C.Finalize(SHA256);
         SHA256C.Reset();
 
         Finished=true;
-        for (uint8_t CheckByte = 0; CheckByte < Pattern.size()-PatternOdd; CheckByte++) Finished=Finished and Pattern[CheckByte]==SHA256[31-CheckByte];
-        if (PatternOdd) Finished=Finished and Pattern.back()==SHA256[32-Pattern.size()]>>4;
-        if (Finished) std::call_once(EnsureOnly1Exit, Exit, TXn);
+        for (CheckByte=0;CheckByte<PatternLen/2;CheckByte++) Finished=Finished and Pattern[CheckByte]==SHA256[31-CheckByte];
+        if (PatternLen%2) Finished=Finished and Pattern[CheckByte]==SHA256[31-CheckByte]>>4;
+        if (Finished) std::call_once(EnsureOnly1Exit, Exit, TXn, TXSize);
     TXn[Pos+7]++;}while(TXn[Pos+7]);    //This byte changes the most.
     TXn[Pos+6]++;}while(TXn[Pos+6]);
     TXn[Pos+5]++;}while(TXn[Pos+5]);
@@ -34,30 +51,16 @@ void Hasher(uint8_t ThreadN, size_t nThreads, const size_t &Pos, const std::vect
     TXn[Pos+3]++;}while(TXn[Pos+3]);
     TXn[Pos+2]++;}while(TXn[Pos+2]);
     TXn[Pos+1]++;}while(TXn[Pos+1]);
-    TXn[Pos]+=nThreads;}while(TXn[Pos]>=nThreads);    //Finish if passed 255.
+    TXn[Pos]+=ThreadsN;}while(TXn[Pos]>=ThreadsN);    //Finish if passed 255.
 }
-uint8_t hexList[103] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,0,0,0,0,0,0,0,0xa,0xb,0xc,0xd,0xe,0xf,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0xa,0xb,0xc,0xd,0xe,0xf};
-std::vector<uint8_t> FromHex(std::string hex){
-    std::vector<uint8_t> ret;
-    for (size_t i = 0; i < hex.size()-1; i += 2) ret.emplace_back(hexList[(uint8_t)hex[i]]<<4 | hexList[(uint8_t)hex[i+1]]);   //People claim std::stringstream is "slow", so I'm using hexList instead.
-    if (hex.size()%2) ret.emplace_back(hexList[(uint8_t)hex.back()]);
-    return ret;
-}
-int main(int argc, char **argv) {
+int main(int argc , char **argv){
     if (argc < 5) {
         std::fprintf(stderr, "Please pass 4 args to this console app, or else don't execute it directly. It's intended for use with a wallet's Python subprocess.Popen");
         std::getchar(); //If anyone double clicks on us, they can read the message.
         return 1;
     }
-    size_t nThreads = FromHex(argv[1])[0] + 1;
-    std::vector<uint8_t> NoncePos = FromHex(argv[2]);
-    size_t Pos = NoncePos[0]<<16 | NoncePos[1]<<8 | NoncePos[2];
-
-    std::vector<uint8_t> Pattern = FromHex(argv[3]);
-    bool PatternOdd = std::string(argv[3]).length() % 2;
-
-    std::vector<uint8_t> TXn = FromHex(argv[4]);
-    std::vector<std::thread> threads;
-    for (uint8_t ThreadN = 0; ThreadN < nThreads; ThreadN++) threads.emplace_back(Hasher, ThreadN, nThreads, Pos, Pattern, PatternOdd, TXn);  //There are many arguments because others seem to care about the "speed" of FromHex.
-    threads[0].join();  // threads[0]'s Hasher could just as well be created here. main is just a Hasher at this point.
+    size_t ThreadsN=FromHex(argv[1])[0]+1;
+    std::thread Threads[ThreadsN];
+    for (uint8_t ThreadN=0;ThreadN<ThreadsN;ThreadN++) Threads[ThreadN]=std::thread(Hasher,ThreadN,argv);
+    Threads[0].join(); // ThreadN 0 is main itself.
 }
